79cb4b35c5cd8b5c3a618616e9fa9ae025d71755
marmo002 <marmo_02@hotmail.com>
2018-06-09 08:14:14 -0400

    New rails app created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    @users = "hello martin"
+  end
+
+  def show
+    #code
+  end
+  
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+end
